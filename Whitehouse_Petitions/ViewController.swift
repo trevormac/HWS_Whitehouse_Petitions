6d18fa02d7472fef694e51926d18ed4660496fd6
trevormac <trevor.c.macgregor@gmail.com>
2017-05-10 22:07:36 -0400

    Initial Commit

---
+++ Whitehouse_Petitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whitehouse_Petitions
+//
+//  Created by Trevor MacGregor on 2017-05-10.
+//  Copyright © 2017 Nusic_Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

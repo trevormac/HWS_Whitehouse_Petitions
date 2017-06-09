b1bf1886af07823cbcafe281fef0a1d4976f2458
trevormac <trevor.c.macgregor@gmail.com>
2017-06-09 15:23:03 -0400

    Refactored to include GCD for background/foreground calls.

--- Whitehouse_Petitions/ViewController.swift
+++ Whitehouse_Petitions/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UITableViewController {
 //Array of dictionaries. Each dict holding a string for its key, and a string for its value
+    var petitions = [[String: String]]()
 
     
     override func viewDidLoad() {
@@ -19,10 +20,50 @@ class ViewController: UITableViewController {
         
     }
     
+    func fetchJSON() {
+        let urlString:String
+        //first vc loads the original Json
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            //detailVC only loads petitions with min 10,000 sigs
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+        //make sure url is valid:
+        if let url = URL(string: urlString) {
+            //create a new data object using contentsOf. returns the contents of the url
+            if let data = try? Data(contentsOf: url) {
+                let json = JSON(data: data)
+                //swiftyJson will return 0 if the values don't exist:
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    self.parse(json: json)
+                    return
+                }
+            }
+        }
+        
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+    
+    
+    //Parse Json into dicts, with each dict having 3 values:title of the petition, body text, and how many signatures
+    func parse(json: JSON){
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue //swiftyJson converst from num to string
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            //adds the new dict to our array
+            petitions.append(obj)
+        }
+        tableView.performSelector(onMainThread: #selector(tableView.reloadData), with: nil, waitUntilDone: false)
+    }
+
+    
     
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return NetworkManager.petitions.count
+        return petitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
